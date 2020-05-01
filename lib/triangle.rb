dfa8c002bc45751dd500c5e9ee181ed6732cc7d3
norio c <norionorioc@gmail.com>
2020-05-01 18:08:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,19 @@ class Triangle
     @c = c
   end 
   
+   def kind 
+    validate_triangle 
+    if a==b && b==c 
+      :equilateral 
+    elsif a==b || b==c || a==c 
+      :isosceles
+    else 
+      :scalene
+    end 
+  end
+  
+  
+  
   def validate_triangle
     real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
     [a, b, c].each do |side|
@@ -20,18 +33,7 @@ class Triangle
   
   
   
-  def kind 
-    validate_triangle 
-    if a==b && b==c 
-      :equilateral 
-    elsif a==b || b==c || a==c 
-      :isosceles
-    else 
-      :scalene
-    end 
-  end
-  
-  
+ 
 end
 
 class TriangleError < StandardError 
